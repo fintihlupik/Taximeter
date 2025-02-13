059789f2e50d9c14c5da0d2727fbaba98313ffef
Polina <53350261+fintihlupik@users.noreply.github.com>
2025-02-13 18:18:50 +0100

    added logs and history

--- OOP Taximeter/Program.py
+++ OOP Taximeter/Program.py
@@ -1,38 +1,49 @@
+#-*- coding: utf-8 -*-
 from Ride import Ride
+import os
 import time
+import logging
 
-# Rates, con round y euro y Control de errores!!!!
-# history (y poner un if en menus) y logs y os clear
+# y logs 
+# autenticacion
+# dependencias y requierements
+# emojis y colorines
+
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+logging.basicConfig(filename='logs/taximeter.log',
+    level=logging.DEBUG, # Establece los niveles que se registrarán de mensajes, nivel DEBUG y superiores.
+    format='%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s', # Establece el formato de los mensajes de log
+    datefmt='%d-%m-%Y %H:%M:%S',
+    encoding="utf-8"
+)
 
 class Program:
 
     def __init__(self):
         self.current_trip = None
+        self.logger = logging.getLogger(self.__class__.__name__)  # Crea un logger con el nombre de Program
 
     def start(self):
         print("Welcome on board!")
         print("The default fare while the taxi is stopped is 2 cents per second.") 
         print("The default fare while the taxi is moving is 5 cents per second.") 
         self.current_trip = Ride()
-        upd_fares = input("Press 0 if you want to change the rates or Enter to use the default ones: ")
-        if upd_fares == '0':
-            while True:
-                try:
-                    self.current_trip.moving_fare  = float(input("Please type the new rate when the taxi is moving: "))
-                    self.current_trip.waiting_fare = float(input("Please type the new rate when the taxi is stopped: "))
-                    if self.current_trip.moving_fare <= 0 or self.current_trip.waiting_fare <= 0:
-                        print("Error. Rates must be greater than 0")
-                    else:
-                        break
-                except ValueError:
-                    print("Invalid input. Please enter a valid number.") 
+        self.current_trip.update_rates()
+        self.logger.info("Ride starts")
+        print ("Enjoy the trip!")
+        time.sleep(0.7)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        self.current_trip.moving()
 
     def welcome(self):
         print("\n***** Welcome to the digital taximeter *****")
         print("This program allows you to calculate taxi fares.")          
     
-    def main_menu(self, custom_options):
-        print("\n Select an option:")
+    def menu(self, custom_options):
+        print("Select an option:")
         options = {
                         't': "Start a new trip",
                         's': "Stop the taxi",
@@ -49,7 +60,16 @@ class Program:
         while option not in custom_options:
             print(f"Invalid option {option}")
             option = input("Choose an option: ")
+            self.logger.warning(f"Invalid input")
+        os.system('cls' if os.name == 'nt' else 'clear')
         return option
+    
+    def view_history(self):
+        os.system('cls' if os.name == 'nt' else 'clear')      
+        history_path = os.path.abspath("logs/rides_history.txt")
+        os.startfile(history_path) # para abrirlo con el editor del texto predeterminado del sistema
+        self.logger.info(f"Showing the history")
+        os.system('cls' if os.name == 'nt' else 'clear')
 
     def main(self):
         # si autenticar devolvio true sigo el flujo
@@ -57,28 +77,36 @@ class Program:
         while True:
             if history != True:
                 self.welcome()
-                option = self.main_menu(["t", "q"])
+                option = self.menu(["t", "q"])
             while True:
                 if option == 't':
                     self.start()
-                    option = self.main_menu(["s", "e", "q"])
+                    option = self.menu(["s", "e", "q"])
                 elif option == 'm':
                     self.current_trip.moving()
-                    option = self.main_menu(["s", "e", "q"])
+                    option = self.menu(["s", "e", "q"])
                 elif option == 's':
                     self.current_trip.waiting()
-                    option = self.main_menu(["m", "e", "q"])
+                    option = self.menu(["m", "e", "q"])
                 elif option == 'e':
                     self.current_trip.calculate_fare()
                     break
                 elif option == 'h':
-                    print("Mostrar historico")
+                    self.view_history()
                     break #temporal
                 elif option == 'q':
+                    print("Goodbye")
+                    time.sleep(0.5)
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                    self.logger.info(f"Exiting the system")
                     exit()
             history = True
-            option = self.main_menu(['t', 'h', 'q'])
+            option = self.menu(['t', 'h', 'q'])
             if option == 'q':
+                print("Goodbye")
+                time.sleep(0.5)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                self.logger.info(f"Exiting the system")
                 break
 
 if __name__ == "__main__":
--- OOP Taximeter/Ride.py
+++ OOP Taximeter/Ride.py
@@ -1,4 +1,8 @@
 import time
+import locale
+import os
+import datetime
+import logging
 
 class Ride:
     def __init__(self):
@@ -8,7 +12,7 @@ class Ride:
         self.start_waiting = None
         self.moving_fare = 0.05
         self.waiting_fare = 0.02
-
+        self.logger = logging.getLogger(self.__class__.__name__)  # Crea un logger con el nombre de Program
 
     def moving(self):
         if self.start_moving is None:
@@ -21,7 +25,10 @@ class Ride:
         input("Press Enter to stop moving ")
         move_time = time.time()-self.start_moving
         self.time_moving += move_time
-        print(f"The taxi was moving for {move_time}")
+        print(f"The taxi was moving for {move_time:.2f} seconds")
+        time.sleep(1.5)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        self.logger.info(f"Taxi in movement")
 
     def waiting(self):
         if self.start_waiting is None:
@@ -29,18 +36,50 @@ class Ride:
             print("The taxi is waiting!!") #log
         self.waiting_time()
         self.start_waiting = None
+        os.system('cls' if os.name == 'nt' else 'clear')
     
     def waiting_time(self):
         input("Press Enter to stop waiting ")
         wait_time = time.time()-self.start_waiting
         self.time_waiting += wait_time
-        print(f"The taxi was waiting for {wait_time}")
+        print(f"The taxi was waiting for {wait_time:.2f} seconds")
+        time.sleep(1.5)
+
+    def update_rates(self):
+        upd_fares = input("Press 0 if you want to change the rates or Enter to use the default ones: ")
+        if upd_fares == '0':
+            while True:
+                try:
+                    self.moving_fare  = float(input("Please type the new rate when the taxi is moving: "))
+                    self.waiting_fare = float(input("Please type the new rate when the taxi is stopped: "))
+                    if self.moving_fare <= 0 or self.waiting_fare <= 0:
+                        print("Error. Rates must be greater than 0")
+                    else:
+                        break
+                except ValueError:
+                    print("Invalid input. Please enter a valid number.")
+        self.logger.info(f"Rates configured: Stopped={self.waiting_fare}, Moving={self.moving_fare}")
     
     def calculate_fare(self):
-        total_fare = self.time_moving * self.moving_fare + self.time_waiting * self.waiting_fare
-        print(f"Tiempo total de movimiento: {self.time_moving} segundos")
-        print(f"Tiempo total de espera: {self.time_waiting} segundos")
-        print(f"Tarifa total: ${total_fare:.2f}")
+        locale.setlocale(locale.LC_ALL, '')
+        moving_fare = self.time_moving * self.moving_fare
+        stopped_fare = self.time_waiting * self.waiting_fare
+        total_fare = locale.currency(stopped_fare + moving_fare)
+        print(f"Total movement time: {self.time_moving:.2f} segundos")
+        print(f"Total waiting time: {self.time_waiting:.2f} segundos")
+        print(f"Total fare: {total_fare}")
+        self.save_trip(total_fare)
+        time.sleep(2.7)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        self.logger.debug(f"Trip ended. Stopped fare={stopped_fare:.2f} sec, Moving fare={moving_fare:.2f} sec, Total={total_fare}")
+
+    def save_trip(self, total_fare):
+            if not os.path.exists("logs"):
+                os.makedirs("logs")
+            with open("logs/rides_history.txt", "a", encoding="utf-8") as file:
+                file.write(f"Ride on {datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')} - Total fare: {total_fare}\n")
+            self.logger.info(f"Trip added to history")
+
 
     
 
